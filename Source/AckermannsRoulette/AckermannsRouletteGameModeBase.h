1972304e006b862efc00917ea4d412a54c6e79dd
walidfstw-hub <63185715+walidfstw-hub@users.noreply.github.com>
2022-12-26 19:15:40 +0400

    First Commit

---
+++ Source/AckermannsRoulette/AckermannsRouletteGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AckermannsRouletteGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACKERMANNSROULETTE_API AAckermannsRouletteGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
